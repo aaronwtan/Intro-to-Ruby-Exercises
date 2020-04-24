eea0582c272de41bbda2f605b11e2a6583ba6395
aaronwtan <18561712+aaronwtan@users.noreply.github.com>
2020-04-24 16:03:45 -0500

    Added Exercise 5

---
+++ The Basics/Ex_5.rb
@@ -0,0 +1,11 @@
+# Write a program that outputs the factorial of the numbers 5, 6, 7, and 8.
+
+fact5 = 1 * 2 * 3 * 4 * 5
+fact6 = fact5 * 6
+fact7 = fact6 * 7
+fact8 = fact7 * 8
+
+puts fact5
+puts fact6
+puts fact7
+puts fact8
