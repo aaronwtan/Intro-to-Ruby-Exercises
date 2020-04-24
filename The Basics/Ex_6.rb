469564d606228865dce3ac5652c283353c1ef9df
aaronwtan <18561712+aaronwtan@users.noreply.github.com>
2020-04-24 16:09:28 -0500

    Added Exercise 6

---
+++ The Basics/Ex_6.rb
@@ -0,0 +1,9 @@
+# Write a program that calculates the squares of 3 float numbers of your choosing and outputs the result to the screen.
+
+a = 3.14
+b = 6.35
+c = 42352435234663.24325234623
+
+puts a ** 2
+puts b ** 2
+puts c ** 2
