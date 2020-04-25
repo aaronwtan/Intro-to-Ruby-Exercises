681639fc671d795fa89a9aa182ce11653ef205fb
aaronwtan <18561712+aaronwtan@users.noreply.github.com>
2020-04-24 21:59:41 -0500

    Added Variables directory with age.rb and name.rb files

---
+++ Variables/name.rb
@@ -0,0 +1,4 @@
+# Asks the user to type in their name and then prints out a greeting message with their name included
+
+puts "Please enter your name: "
+puts "Hello there, " + gets.chomp + "! General Kenobi!"
