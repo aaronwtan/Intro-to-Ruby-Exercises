014be5a092aed08af14bef9a868847a8d851c6ab
aaronwtan <18561712+aaronwtan@users.noreply.github.com>
2020-04-24 22:25:58 -0500

    Updated name.rb

--- Variables/name.rb
+++ Variables/name.rb
@@ -1,13 +1,11 @@
 # Asks the user to type in their name and then prints out a greeting message with their name included.
 
-puts "Please enter your name: "
-name = gets.chomp
-puts "Hello there, #{name}! General Kenobi!"
+puts "Please enter your first name: "
+first_name = gets.chomp
+puts "Please enter your last name: "
+last_name = gets.chomp
+puts "Hello there, #{first_name} #{last_name}! General Kenobi!"
 
 # name.rb continued: Print the name of the user 10 times without explicitly writing the puts method 10 times in a row.
 
-for i in 1..5
-  puts name
-end
-
-5.times{puts name}
+10.times{puts first_name + " " + last_name}
