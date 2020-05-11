1d05ea982049fccdc1df12f5066ca0b5c429f83f
aaronwtan <18561712+aaronwtan@users.noreply.github.com>
2020-05-11 16:41:43 -0500

    Updated Ex_3.rb

--- Flow Control/Ex_3.rb
+++ Flow Control/Ex_3.rb
@@ -5,9 +5,9 @@ num = gets.chomp.to_i
 
 if num < 0
   puts "#{num} is a negative number bruh"
-elsif num >= 0 && num <= 50
+elsif num <= 50
   puts "#{num} is between 0 and 50"
-elsif num >= 51 && num <= 100
+elsif num <= 100
   puts "#{num} is between 51 and 100"
 else
   puts "#{num} is above 100"
---
+++ Flow Control/Ex_4.rb
@@ -0,0 +1,1 @@
+# What will each block of code below print to the screen?
