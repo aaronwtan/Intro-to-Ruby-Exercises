6ba41df703c5c7ade7997892d5587e542ab1afe3
aaronwtan <18561712+aaronwtan@users.noreply.github.com>
2020-05-12 17:51:23 -0500

    Added Ex_6.rb

---
+++ Hashes/Ex_6.rb
@@ -0,0 +1,9 @@
+# Given the following code...
+
+x = "hi there"
+my_hash = {x: "some value"}
+my_hash2 = {x => "some value"}
+
+# What's the difference between the two hashes that were created?
+
+# In my_hash, the symbol x was used as the key, while in my_hash2, the string value of the variable x was used as the key
