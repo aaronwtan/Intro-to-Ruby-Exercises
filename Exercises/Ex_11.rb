09b933a9b9df1f40fbea1c5679af65cc02e15d8d
aaronwtan <18561712+aaronwtan@users.noreply.github.com>
2020-05-13 22:48:53 -0500

    Added Ex_11.rb

---
+++ Exercises/Ex_11.rb
@@ -0,0 +1,36 @@
+# Given the following data structures. Write a program that copies the information from the array into the empty hash that applies to the correct person.
+
+contact_data = [["joe@email.com", "123 Main st.", "555-123-4567"],
+            ["sally@email.com", "404 Not Found Dr.", "123-234-3454"]]
+
+contacts = {"Joe Smith" => {}, "Sally Johnson" => {}}
+
+# Expected output:
+#  {
+#    "Joe Smith"=>{:email=>"joe@email.com", :address=>"123 Main st.", :phone=>"555-123-4567"},
+#    "Sally Johnson"=>{:email=>"sally@email.com", :address=>"404 Not Found Dr.",  :phone=>"123-234-3454"}
+#  }
+
+info = [:email, :address, :phone]
+person_ind = 0
+
+contacts.each_value do |value|
+  for i in 0...info.length do
+    value[info[i]] = contact_data[person_ind][i]
+  end
+  person_ind += 1
+end
+
+p contacts
+
+
+# or manually
+
+# contacts["Joe Smith"][:email] = contact_data[0][0]
+# contacts["Joe Smith"][:address] = contact_data[0][1]
+# contacts["Joe Smith"][:phone] = contact_data[0][2]
+# contacts["Sally Johnson"][:email] = contact_data[1][0]
+# contacts["Sally Johnson"][:address] = contact_data[1][1]
+# contacts["Sally Johnson"][:phone] = contact_data[1][2]
+
+# p contacts
