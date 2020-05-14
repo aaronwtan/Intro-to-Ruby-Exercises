f043f0c0bb29e9e4750c533cce479866ac20ceba
aaronwtan <18561712+aaronwtan@users.noreply.github.com>
2020-05-13 22:19:01 -0500

    Added Ex_9.rb and Ex_10.rb

---
+++ Exercises/Ex_10.rb
@@ -0,0 +1,8 @@
+# Can hash values be arrays? Can you have an array of hashes? (give examples)
+# yes; yes
+
+# hash values as arrays
+p h = {key: ["This", "is", "an", "array", 1, "DUH"], key2: ["lol", 42]}
+
+# array of hashes
+p arr = [{key: "I'm a hash within an array!"}, {key: "Me too!"}, {key: 42}]
